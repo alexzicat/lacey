9e6517b0ef8bc0f28b6cd9538c701745f7f1cd97
alexzicat <dev.alexzicat@gmail.com>
2016-01-14 17:05:47 -0500

    LaceyModule - inheritance

--- lacey.js
+++ lacey.js
@@ -1,41 +1,56 @@
 (function () {
   'use strict';
 
-  var LaceyModule, validate_app_name, validate_module_name, validate_module_type, validate_duplicated_module;
+  var LaceyApp,
+    LaceyModule,
+    modules,
+    validate_app_name,
+    validate_module_name,
+    validate_module_type,
+    validate_duplicated_module;
 
   LaceyApp = function (app_name) {
-    validate_app_name(app_name);
+    validate_app_name.call(this, app_name);
 
-    this.modules = {};
+    modules = this.modules = {};
     this.name = app_name;
 
     return this;
   };
 
-  LaceyApp.prototype.register_module = function (module_name, module) {
-    validate_module_name.call(this, module_name);
-    validate_module_type.call(this, module);
-    validate_duplicated_module.call(this, module_name);
-
-    this.modules[module_name] = module;
-
-    this[module_name] = new LaceyModule(module_name, module);
-  };
-
-  // private
-
-  LaceyModule = function (module_name, module) {
-    this.instance = null;
-    this.module = module;
-    this.module_name = module_name;
-  };
-
-  LaceyModule.prototype.get_instance = function () {
-    if (this.instance === null) {
-      this.instance = new this.module();
+  LaceyApp.prototype.register_module = function (module_name, parent_module, Module) {
+    if (typeof parent_module === 'function') {
+      Module = parent_module;
+      parent_module = null;
     }
 
-    return this.instance;
+    validate_module_name.call(this, module_name);
+    validate_module_type.call(this, Module);
+    validate_duplicated_module.call(this, module_name);
+
+    this[module_name] = new LaceyModule(module_name, parent_module, Module);
+    this.modules[module_name] = Module;
+
+    return this[module_name];
+  };
+
+  LaceyModule = function (module_name, parent_module, Module) {
+    var instance = null;
+
+    this.module_name = module_name;
+
+    this.get_instance = function () {
+      if (instance === null) {
+        if (parent_module !== null) {
+          Module.prototype = new modules[parent_module]();
+          Module.prototype.constructor = Module;
+        }
+
+        instance = new Module();
+      }
+
+      return instance;
+    };
   };
 
   LaceyModule.prototype.initialize = function () {
@@ -50,28 +65,37 @@
     return this.instance;
   };
 
-
   validate_app_name = function (app_name) {
     if (typeof app_name !== 'string' || app_name === '') {
       throw 'InvalidNameError - you must give a valid name to your lacey app';
     }
+
+    return true;
   };
 
   validate_module_name = function (module_name) {
     if (typeof module_name !== 'string' || module_name === '') {
       throw 'InvalidModuleNameError - you must give a valid name to your module';
     }
+
+    return true;
   };
 
-  validate_module_type = function (module) {
-    if (typeof module !== 'function') {
+  validate_module_type = function (Module) {
+    if (typeof Module !== 'function') {
       throw 'InvalidModuleError - your module must be a function';
     }
+
+    return true;
   };
 
   validate_duplicated_module = function (module_name) {
     if (this.modules[module_name] !== undefined) {
       throw 'DuplicateModuleError - your module has already been registered';
     }
+
+    return true;
   };
+
+  window.LaceyApp = LaceyApp;
 }());
