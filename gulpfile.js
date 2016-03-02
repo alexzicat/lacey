d27c87b66ec68395e257c28880222d532aa6d556
alexzicat <dev.alexzicat@gmail.com>
2016-03-02 15:50:01 -0500

    Karma + Jasmine

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
   var gulp = require('gulp'),
     concat = require('gulp-concat'),
     insert = require('gulp-insert'),
+    KarmaServer = require('karma').Server,
     src = {
       'scripts': ['./app/javascripts/core-ext/array.js',
                   './app/javascripts/core-ext/object.js',
@@ -12,9 +13,24 @@
 
   gulp.task('concat', function () {
     return gulp.src(src.scripts)
-      .pipe(concat('lacey.js', {newLine: '\n\n'}))
+      .pipe(concat('lacey.js', {
+        newLine: '\n\n'
+      }))
       .pipe(insert.append('\n\nwindow.LaceyApp = LaceyApp;'))
       .pipe(gulp.dest('./dist/'));
+  });
+
+  gulp.task('test', function (done) {
+    new KarmaServer({
+      configFile: __dirname + '/karma.conf.js',
+      singleRun: true
+    }, done).start();
+  });
+
+  gulp.task('tdd', function (done) {
+    new KarmaServer({
+      configFile: __dirname + '/karma.conf.js'
+    }, done).start();
   });
 
   gulp.task('default', function () {});
